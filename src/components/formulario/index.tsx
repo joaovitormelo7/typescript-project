b276c24f494f4a220c3691c0004d0bf80edc47cc
João Vitor de Sá Melo <joaovitormelo199@gmail.com>
2024-05-22 17:41:22 -0300

    first commit

---
+++ src/components/formulario/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Botao from '../button';
+
+class Formulario extends React.Component {
+    render() {
+        return(
+            <form>
+
+                <div>
+                    <label htmlFor="tarefa">
+                        Adicione um novo estudo
+                    </label>
+                        <input 
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        placeholder="O que você quer estudar"
+                        required
+                         />
+                    
+                </div>
+
+                <div>
+                    <label htmlFor="tempo">
+                        Tempo
+                        <input 
+                        type="time" 
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                        />
+                    </label>
+                </div>
+
+                <Botao />
+            </form>
+        )
+        
+    }
+}
+
+export default Formulario;
