adb1defd0dfe0eca1be34e5ee49a652a352f04cc
João Vitor de Sá Melo <joaovitormelo199@gmail.com>
2024-05-27 20:17:48 -0300

    project

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
 import style from './Lista.module.scss';
+import Item from './item';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }]
+function Lista({ tarefas }: { tarefas: Array<ITarefa>}) {
     return(
         <aside className ={style.listaTarefas}>
-            <h2>Estudos do dia </h2>
+            <h2> Estudos do dia </h2>
             <ul>
             {tarefas.map((item, index) => (
-                    <li key={index} className = {style.item}>
-                        <h3>{item.tarefa} </h3>
-                        <span>{item.tempo} </span>
-                    </li>
+                <Item
+                    key={index}
+                    {...item}
+                />
                 ))}   
             </ul>
         </aside>
